9153dbe317dd43937b83414c48d9a411e1f023eb
achigit <achi.wgn@gmail.com>
2016-01-13 03:07:36 +0200

    Crazy code to follow and do tavlat maakav

---
+++ lesson_2/TavlatMaakav.java
@@ -0,0 +1,36 @@
+public static void run(){
+	int x = 4;
+	int y,z,qwe;
+	String myString, anotherString, mul;
+	
+	mul = "ab";
+	z = -15;
+	y = 0;
+	while ( y < x ){
+		if ( x > 2 ){
+			my_string = "Hello ";
+		}
+		else {
+			my_string = "Bye ";
+		}
+	
+		anotherString = myString + mul
+		
+		
+		
+		if ( y/2==x/4 ){
+			z = 5;
+		}
+		else{
+			z = z*2
+		}
+		
+		if ( z > 19 ){
+			while ( z > 5 ){
+				anotherString = anotherString + mul
+				z = z-2;
+			}
+		}
+		x = x + (z*y)
+		y = y+1;
+}
