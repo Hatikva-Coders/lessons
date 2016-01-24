89d07cafb24d76445942bfc9790f5c07e1b07af9
aryehpro <aryehpro@gmail.com>
2016-01-25 00:58:25 +0200

    added simple console example program

---
+++ lesson_4/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * Exapmle simple console program.
+ *
+ * 
+ * Created by Aryeh on 25/01/2016.
+ */
+public class Hello {
+
+    public static void main(String [] args) {
+		
+		System.out.println("Hello Hatikva!");
+		
+	}
+
+}
